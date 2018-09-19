d287a5c4bdfe29921a415855b654085e4aa5bd68
David Kavanaugh <dkavanaugh@psgastro.com>
2018-09-19 15:24:17 -0700

    header-logo works!, header-navigation works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderLogoComponent } from './shared/components/header/header-logo/header-logo.component';
+import { HeaderNavigationComponent } from './shared/components/header/header-navigation/header-navigation.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     FooterComponent,
     Project1Component,
     Project2Component,
-    Project3Component
+    Project3Component,
+    HeaderLogoComponent,
+    HeaderNavigationComponent
   ],
   imports: [
     BrowserModule,
