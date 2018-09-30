93941a1cea17461be7eef04a645d6efe18b0ff51
David Kavanaugh <david.kavanaugh@me.com>
2018-09-30 13:05:55 -0700

    adding styles

--- src/app/modules/project3/project3.module.ts
+++ src/app/modules/project3/project3.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Project3Component } from './pages/project3/project3.component';
+import { Project3Component } from './pages/project3/project3.page';
 
 @NgModule({
   imports: [
